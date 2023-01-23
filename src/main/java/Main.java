beb57bd31bcfbdc52c04c51b1ea58c21cab3e400
AmaalAljasria <a26j123@gmail.com>
2023-01-23 16:53:11 +0400

    add new

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,6 +16,7 @@ public class Main {
 	static final String DB_URL = "jdbc:mysql://localhost:3306/maveindbms3";
 	static final String USER = "root";
 	static final String PASS = "root";
+
 	static void choicesFunction1() {
 
 		System.out.println("***************************");
@@ -28,8 +29,9 @@ public class Main {
 		System.out.println("6. Creat books table");
 		System.out.println("7. Insert to Table books");
 		System.out.println("8. create Authors Table");
-		System.out.println("9. update from Table (domains based in id)");
-		System.out.println("10. delete from Table web based in id");
+		System.out.println("9. Insert to Authors Table");
+		System.out.println("10. Create Table Articals");
+		System.out.println("11. Insert to Table Artical");
 	}
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
@@ -52,9 +54,10 @@ public class Main {
 
 			case 2:
 				System.out.println("==========Read Api from Artical Table===================");
-	              
-				URL url = new URL("https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=romney&facet_field=day_of_week&facet=true&begin_date=20120101&end_date=20120101&api-key=IO0i2IlGBNmzuUbAmzcAPdzPH5LcPss2\r\n"
-						+ "");
+
+				URL url = new URL(
+						"https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=romney&facet_field=day_of_week&facet=true&begin_date=20120101&end_date=20120101&api-key=IO0i2IlGBNmzuUbAmzcAPdzPH5LcPss2\r\n"
+								+ "");
 				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 				conn.setRequestMethod("GET");
 				conn.connect();
@@ -75,11 +78,11 @@ public class Main {
 					System.out.println(informationString.toString());
 
 				}
-				
-				
+
 				System.out.println("==========Read Api from Authors Table===================");
-				URL url1 = new URL("https://api.nytimes.com/svc/books/v3/reviews.json?author=Stephen+King&api-key=IO0i2IlGBNmzuUbAmzcAPdzPH5LcPss2\r\n"
-						+ "");
+				URL url1 = new URL(
+						"https://api.nytimes.com/svc/books/v3/reviews.json?author=Stephen+King&api-key=IO0i2IlGBNmzuUbAmzcAPdzPH5LcPss2\r\n"
+								+ "");
 				HttpURLConnection conn1 = (HttpURLConnection) url1.openConnection();
 				conn1.setRequestMethod("GET");
 				conn1.connect();
@@ -101,12 +104,11 @@ public class Main {
 
 				}
 				System.out.println("===========================================");
-				
-				
-				
+
 				System.out.println("==========Read Api from Section Books Table===================");
-				URL url11 = new URL("https://api.nytimes.com/svc/topstories/v2/books.json?api-key=IO0i2IlGBNmzuUbAmzcAPdzPH5LcPss2\r\n"
-						+ "");
+				URL url11 = new URL(
+						"https://api.nytimes.com/svc/topstories/v2/books.json?api-key=IO0i2IlGBNmzuUbAmzcAPdzPH5LcPss2\r\n"
+								+ "");
 				HttpURLConnection conn11 = (HttpURLConnection) url11.openConnection();
 				conn11.setRequestMethod("GET");
 				conn11.connect();
@@ -129,13 +131,13 @@ public class Main {
 				}
 				System.out.println("===========================================");
 				choicesFunction1();
-				
-				
+
 				break;
-				
+
 			case 3:
 				System.out.println("==========Read from API by Books class===================");
-				URL url111 = new URL("https://api.nytimes.com/svc/topstories/v2/books.json?api-key=IO0i2IlGBNmzuUbAmzcAPdzPH5LcPss2");
+				URL url111 = new URL(
+						"https://api.nytimes.com/svc/topstories/v2/books.json?api-key=IO0i2IlGBNmzuUbAmzcAPdzPH5LcPss2");
 				HttpURLConnection conn111 = (HttpURLConnection) url111.openConnection();
 				conn111.setRequestMethod("GET");
 				conn111.connect();
@@ -155,31 +157,30 @@ public class Main {
 					Gson gson = new Gson();
 					// System.out.println(informationString1.toString());
 					Root apiResult = gson.fromJson(informationString111.toString(), Root.class);
-					//Result[] apiResult1 = gson.fromJson(informationString111.toString(), Result[].class);
-				
-					
-							//System.out.println("STATUS : " + apiResult.getStatus());
-							for (int i=0; i<informationString111.length();i++) {
-								
-							System.out.println("Section : " + apiResult.getSection());
-							System.out.println("Title : " + apiResult.getResults()[0].getTitle());
-							System.out.println("Published_date :"+ apiResult.getResults()[0].getPublished_date());
-							System.out.println("Des_facet :"+ apiResult.getResults()[0].getDes_facet());
+					// Result[] apiResult1 = gson.fromJson(informationString111.toString(),
+					// Result[].class);
 
+					// System.out.println("STATUS : " + apiResult.getStatus());
+					for (int i = 0; i < informationString111.length(); i++) {
 
-		
-							System.out.println("|" + " ***************************** " + "|");
-							}
-					
+						System.out.println("Section : " + apiResult.getSection());
+						System.out.println("Title : " + apiResult.getResults()[0].getTitle());
+						System.out.println("Published_date :" + apiResult.getResults()[0].getPublished_date());
+						System.out.println("Des_facet :" + apiResult.getResults()[0].getDes_facet());
+
+						System.out.println("|" + " ***************************** " + "|");
+					}
+
 				}
 
 				System.out.println("===========================================");
 				choicesFunction1();
 				break;
-				
+
 			case 4:
 				System.out.println("==========Read from API by Authors class===================");
-				URL url1111 = new URL("https://api.nytimes.com/svc/books/v3/reviews.json?author=Stephen+King&api-key=IO0i2IlGBNmzuUbAmzcAPdzPH5LcPss2");
+				URL url1111 = new URL(
+						"https://api.nytimes.com/svc/books/v3/reviews.json?author=Stephen+King&api-key=IO0i2IlGBNmzuUbAmzcAPdzPH5LcPss2");
 				HttpURLConnection conn1111 = (HttpURLConnection) url1111.openConnection();
 				conn1111.setRequestMethod("GET");
 				conn1111.connect();
@@ -199,30 +200,28 @@ public class Main {
 					Gson gson = new Gson();
 					// System.out.println(informationString1.toString());
 					Root apiResult = gson.fromJson(informationString1111.toString(), Root.class);
-					//Result[] apiResult1 = gson.fromJson(informationString111.toString(), Result[].class);
-				
-					
-							//System.out.println("STATUS : " + apiResult.getStatus());
-							for (int i=0; i<informationString1111.length();i++) {
-								
-							System.out.println("Book Authors : " + apiResult.getResults()[0].getBook_author());
-							
+					// Result[] apiResult1 = gson.fromJson(informationString111.toString(),
+					// Result[].class);
 
-		
-							System.out.println("|" + " ***************************** " + "|");
-							}
-					
+					// System.out.println("STATUS : " + apiResult.getStatus());
+					for (int i = 0; i < informationString1111.length(); i++) {
+
+						System.out.println("Book Authors : " + apiResult.getResults()[0].getBook_author());
+
+						System.out.println("|" + " ***************************** " + "|");
+					}
+
 				}
 
 				System.out.println("===========================================");
 				choicesFunction1();
 				break;
-				
+
 			case 5:
 				System.out.println("==========Read from API by Articals class===================");
-				URL url11111 = new URL("https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=romney&facet_field=day_of_week&facet=true&begin_date=20120101&end_date=20120101&api-key=IO0i2IlGBNmzuUbAmzcAPdzPH5LcPss2\r\n"
-						+ ""
-						+ "");
+				URL url11111 = new URL(
+						"https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=romney&facet_field=day_of_week&facet=true&begin_date=20120101&end_date=20120101&api-key=IO0i2IlGBNmzuUbAmzcAPdzPH5LcPss2\r\n"
+								+ "" + "");
 				HttpURLConnection conn11111 = (HttpURLConnection) url11111.openConnection();
 				conn11111.setRequestMethod("GET");
 				conn11111.connect();
@@ -242,27 +241,31 @@ public class Main {
 					Gson gson = new Gson();
 					// System.out.println(informationString1.toString());
 					Artical apiResult1 = gson.fromJson(informationString11111.toString(), Artical.class);
-					//Result[] apiResult1 = gson.fromJson(informationString111.toString(), Result[].class);
-					
-					
-							//System.out.println("STATUS : " + apiResult.getStatus());
-							for (int i=0; i<=apiResult1.getResponse().getdocs().length;i++) {
-								
-							System.out.println("pub_date : "+apiResult1.getResponse().getdocs()[0].getPub_date());
-							System.out.println("Document_type : "+apiResult1.getResponse().getdocs()[0].getDocument_type());
-							System.out.println("Section_name : "+apiResult1.getResponse().getdocs()[0].getSection_name());
-							System.out.println("Subsection_name : "+apiResult1.getResponse().getdocs()[0].getSubsection_name());
-							System.out.println("fIRST name : "+apiResult1.getResponse().getdocs()[0].getByline().getperson()[0].getFirstname());
-							System.out.println("last name : "+apiResult1.getResponse().getdocs()[0].getByline().getperson()[0].getMiddlename());
-							System.out.println("|" + " ***************************** " + "|");
-							}
-					
+					// Result[] apiResult1 = gson.fromJson(informationString111.toString(),
+					// Result[].class);
+
+					// System.out.println("STATUS : " + apiResult.getStatus());
+					for (int i = 0; i <= apiResult1.getResponse().getdocs().length; i++) {
+
+						System.out.println("pub_date : " + apiResult1.getResponse().getdocs()[0].getPub_date());
+						System.out
+								.println("Document_type : " + apiResult1.getResponse().getdocs()[0].getDocument_type());
+						System.out.println("Section_name : " + apiResult1.getResponse().getdocs()[0].getSection_name());
+						System.out.println(
+								"Subsection_name : " + apiResult1.getResponse().getdocs()[0].getSubsection_name());
+						System.out.println("fIRST name : "
+								+ apiResult1.getResponse().getdocs()[0].getByline().getperson()[0].getFirstname());
+						System.out.println("last name : "
+								+ apiResult1.getResponse().getdocs()[0].getByline().getperson()[0].getMiddlename());
+						System.out.println("|" + " ***************************** " + "|");
+					}
+
 				}
 
 				System.out.println("===========================================");
 				choicesFunction1();
 				break;
-				
+
 			case 6:
 				System.out.println("==========Create books Table===================");
 				CrateAllTable CreateAllTable = new CrateAllTable();
@@ -270,10 +273,11 @@ public class Main {
 				System.out.println("================================================");
 				choicesFunction1();
 				break;
-			
+
 			case 7:
 				System.out.println("==========Insert to  books Table===================");
-				URL url111111 = new URL("https://api.nytimes.com/svc/topstories/v2/books.json?api-key=IO0i2IlGBNmzuUbAmzcAPdzPH5LcPss2");
+				URL url111111 = new URL(
+						"https://api.nytimes.com/svc/topstories/v2/books.json?api-key=IO0i2IlGBNmzuUbAmzcAPdzPH5LcPss2");
 				HttpURLConnection conn111111 = (HttpURLConnection) url111111.openConnection();
 				conn111111.setRequestMethod("GET");
 				conn111111.connect();
@@ -293,17 +297,15 @@ public class Main {
 					Gson gson = new Gson();
 					// System.out.println(informationString1.toString());
 					Root apiResult = gson.fromJson(informationString111111.toString(), Root.class);
-					for (int i=0; i<=apiResult.getSection().length();i++) {
-				
-						
-						String Section = apiResult.getSection();
+					for (int i = 0; i <= apiResult.getSection().length(); i++) {
+
+						String Section = apiResult.getResults()[0].getBook_author();
 						String Title = apiResult.getResults()[0].getTitle();
 						String Published_date = apiResult.getResults()[0].getPublished_date();
 						ArrayList<String> Des_facet = apiResult.getResults()[0].getDes_facet();
-						
 
 						Class.forName("com.mysql.cj.jdbc.Driver");
-						String url1111111= "jdbc:mysql://localhost:3306/maveindbms3";
+						String url1111111 = "jdbc:mysql://localhost:3306/maveindbms3";
 						String username = "root";
 						String password = "root";
 
@@ -312,8 +314,8 @@ public class Main {
 
 						if (con1 != null) {
 							String SQLqueryForInserting = "insert into books(Section,Title, Published_date,Des_facet)"
-									+ " values('" + Section + "' ,'" + Title + "', '" + Published_date
-									+ "','" + Des_facet +  "')";
+									+ " values('" + Section + "' ,'" + Title + "', '" + Published_date + "','"
+									+ Des_facet + "')";
 							Statement st = con1.createStatement();
 							//
 							// Executing query
@@ -332,16 +334,15 @@ public class Main {
 					}
 				}
 
-				
+				choicesFunction1();
 				break;
-				
+
 			case 8:
 				System.out.println("==========Create Authors Table===================");
 				try (Connection conn1111111 = DriverManager.getConnection(DB_URL, USER, PASS);
 						Statement stmt = conn1111111.createStatement();) {
 					String sql = "CREATE TABLE BookAuthor " + "(id int NOT NULL AUTO_INCREMENT, "
-							+ " Book_author   VARCHAR(100) not NULL, " +
-							" PRIMARY KEY ( id ))";
+							+ " Book_author   VARCHAR(100) not NULL, " + " PRIMARY KEY ( id ))";
 					String sql1 = "ALTER TABLE books AUTO_INCREMENT=1";
 					stmt.executeUpdate(sql);
 					stmt.executeUpdate(sql1);
@@ -352,9 +353,156 @@ public class Main {
 					System.out.println("Table Authors Table Already Build");
 					System.out.println("***********************************");
 				}
-				
+				choicesFunction1();
 				break;
-				
+
+			case 9:
+				System.out.println("==========Insert to Authors Table===================");
+				URL url1111111 = new URL(
+						"https://api.nytimes.com/svc/books/v3/reviews.json?author=Stephen+King&api-key=IO0i2IlGBNmzuUbAmzcAPdzPH5LcPss2");
+				HttpURLConnection conn1111111 = (HttpURLConnection) url1111111.openConnection();
+				conn1111111.setRequestMethod("GET");
+				conn1111111.connect();
+				StringBuilder informationString1111111 = new StringBuilder();
+				int responseCode1111111 = conn1111111.getResponseCode();
+				if (responseCode1111111 != 200) {
+					throw new RuntimeException("HttpresponseCode");
+
+				}
+
+				else {
+					Scanner scanner = new Scanner(url1111111.openStream());
+					while (scanner.hasNext()) {
+						informationString1111111.append(scanner.nextLine());
+					}
+					scanner.close();
+					Gson gson = new Gson();
+					// System.out.println(informationString1.toString());
+					Root apiResult = gson.fromJson(informationString1111111.toString(), Root.class);
+					for (int i = 0; i <= informationString1111111.length(); i++) {
+
+						String Book_author = apiResult.getResults()[0].getBook_author();
+
+						Class.forName("com.mysql.cj.jdbc.Driver");
+						String url11111111 = "jdbc:mysql://localhost:3306/maveindbms3";
+						String username = "root";
+						String password = "root";
+
+						Connection con1 = DriverManager.getConnection(url11111111, username, password);
+						int id = 1;
+
+						if (con1 != null) {
+							String SQLqueryForInserting = "insert into BookAuthor(Book_author)" + " values('"
+									+ Book_author + "')";
+							Statement st = con1.createStatement();
+							//
+							// Executing query
+							int m = st.executeUpdate(SQLqueryForInserting);
+							if (m >= 1) {
+								System.out.println("inserted successfully : " + SQLqueryForInserting);
+
+							} else {
+								System.out.println("insertion failed");
+
+								// Closing the connections
+							}
+
+						}
+
+					}
+				}
+				choicesFunction1();
+				break;
+			case 10:
+				System.out.println("==========Create Articals Table===================");
+				try (Connection conn11111111 = DriverManager.getConnection(DB_URL, USER, PASS);
+						Statement stmt = conn11111111.createStatement();) {
+					String sql = "CREATE TABLE Artical " + "(id INTEGER not NULL, "
+							+ "Pub_date  VARCHAR(100)  ,"
+							+ "FOREIGN KEY (Pub_date) REFERENCES books(Published_date) ON DELETE CASCADE ,"
+							+ " Document_type   VARCHAR(100) not NULL, " +" Section_name   VARCHAR(100) not NULL, "
+							+ " Subsection_name   VARCHAR(100) not NULL, " +" Firstname   VARCHAR(100) not NULL, "
+							+ " Middlename   VARCHAR(100) not NULL, " 
+							+ " PRIMARY KEY ( id ))";
+					String sql1 = "ALTER TABLE Artical AUTO_INCREMENT=1";
+					stmt.executeUpdate(sql);
+					stmt.executeUpdate(sql1);
+
+					System.out.println(true + "===>Created Articals Table table in given database...");
+
+				} catch (SQLException e) {
+					System.out.println("Table Articals Table Already Build");
+					System.out.println("***********************************");
+				}
+
+				choicesFunction1();
+				break;
+			case 11:
+				System.out.println("==========Insert to  books Table===================");
+				URL url11111111 = new URL(
+						"https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=romney&facet_field=day_of_week&facet=true&begin_date=20120101&end_date=20120101&api-key=IO0i2IlGBNmzuUbAmzcAPdzPH5LcPss2");
+				HttpURLConnection conn11111111 = (HttpURLConnection) url11111111.openConnection();
+				conn11111111.setRequestMethod("GET");
+				conn11111111.connect();
+				StringBuilder informationString11111111 = new StringBuilder();
+				int responseCode11111111 = conn11111111.getResponseCode();
+				if (responseCode11111111 != 200) {
+					throw new RuntimeException("HttpresponseCode");
+
+				}
+
+				else {
+					Scanner scanner = new Scanner(url11111111.openStream());
+					while (scanner.hasNext()) {
+						informationString11111111.append(scanner.nextLine());
+					}
+					scanner.close();
+					Gson gson = new Gson();
+					// System.out.println(informationString1.toString());
+					Artical apiResult = gson.fromJson(informationString11111111.toString(), Artical.class);
+					for (int i = 0; i <= apiResult.getResponse().getdocs().length; i++) {
+
+						String Pub_date = apiResult.getResponse().getdocs()[0].getPub_date();
+						String Document_type = apiResult.getResponse().getdocs()[0].getDocument_type();
+						String Section_name = apiResult.getResponse().getdocs()[0].getSection_name();
+						String Subsection_name = apiResult.getResponse().getdocs()[0].getSubsection_name();
+						String Firstname = apiResult.getResponse().getdocs()[0].getByline().getperson()[0]
+								.getFirstname();
+						String Middlename = apiResult.getResponse().getdocs()[0].getByline().getperson()[0]
+								.getMiddlename();
+
+						Class.forName("com.mysql.cj.jdbc.Driver");
+						String url111111111 = "jdbc:mysql://localhost:3306/maveindbms3";
+						String username = "root";
+						String password = "root";
+
+						Connection con1 = DriverManager.getConnection(url111111111, username, password);
+						int id = 1;
+
+						if (con1 != null) {
+							String SQLqueryForInserting = "insert into Artical(Pub_date,Document_type, Section_name,Subsection_name,Firstname,Middlename)"
+									+ " values('" + Pub_date + "' ,'" + Document_type + "', '" + Section_name + "','"
+									+ Subsection_name + "','" + Firstname + "','" + Middlename + "')";
+							Statement st = con1.createStatement();
+							//
+							// Executing query
+							int m = st.executeUpdate(SQLqueryForInserting);
+							if (m >= 1) {
+								System.out.println("inserted successfully : " + SQLqueryForInserting);
+
+							} else {
+								System.out.println("insertion failed");
+
+								// Closing the connections
+							}
+
+						}
+
+					}
+				}
+				choicesFunction1();
+				break;
+
 			}
 		} while (true);
 	}
